ec56c2197218f054d0e42885dc0566427ad8dcfa
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-09-20 12:56:15 -0400

    Commit

---
+++ Tupletolist.py
@@ -0,0 +1,34 @@
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
+
+# Tuple have integers
+my_tuple = ("Age", 13)
+print(my_tuple)
+
+# tuple with mixed datatypes
+my_tuple = (1, "Joshua", 3.4)
+print(my_tuple)
+
+# nested tuple
+my_tuple = ("Bidjou", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+# Accessing tuple elements using indexing
+my_tuple = ("Height", [5,4])
+print(my_tuple[0])
+print(my_tuple[5])
+
+# nested tuple
+n_tuple = ("Weight", [140])
+
+# nested index
+print(n_tuple[0] [3])
+print(n_tuple[1] [1])
+
+# Slicing
+print("Sliced :", my_tuple[1:4])
+
+# Iterating through tuple
+for letter in (my_tuple):
+    print("Favourite subject", 'Mathematics')
